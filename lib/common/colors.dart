68a7c7e65b0844782eb33868b9ac1e992b5fb91e
al-mamur-shimul <shimul.vivasoft@bkash.com>
2025-01-14 06:53:14 +0600

    Add required colors for the app

---
+++ lib/common/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static Color mediumHeadingColor = const Color(0xff212020);
+  static Color smallBodyTextColor = const Color(0xff676767);
+}
