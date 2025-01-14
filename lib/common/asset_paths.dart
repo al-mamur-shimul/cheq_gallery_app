9e2c510f1057ec9fc4dd3b14659e436330fe7d00
al-mamur-shimul <shimul.vivasoft@bkash.com>
2025-01-14 07:58:26 +0600

    Add required assets for album grid list page

--- lib/common/asset_paths.dart
+++ lib/common/asset_paths.dart
@@ -2,6 +2,7 @@ class AssetPaths {
   const AssetPaths._();
 
   static const String logo = 'assets/images/logo.png';
+  static const String people = 'assets/images/people.png';
   static const String permissionRequired =
       'assets/images/require_permission.png';
 }
--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -5,4 +5,6 @@ class AppColors {
 
   static Color mediumHeadingColor = const Color(0xff212020);
   static Color smallBodyTextColor = const Color(0xff676767);
+  static Color largeHeadingTextColor = const Color(0xff363636);
+  static Color mutedTextColor = const Color(0xffD8D8D8);
 }
